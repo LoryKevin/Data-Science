5de802f84fdbd8151b5b3e4441b4a68c3595ddbb
LoryKevin <luzhihan_lzh@163.com>
2020-07-30 18:43:16 +0800

    feat: final version

--- src/main/main.py
+++ src/main/main.py
@@ -7,11 +7,11 @@ from src.visualization.plot import *
 
 if __name__ == '__main__':
     print("分析开始！")
-    data_initialize()
-    data_preprocess()
-    code_check()
-    code_remove()
-    get_nums()
-    graph_generate()
+    data_initialize() # 数据初始化，包括文件系统初始化、下载源数据
+    data_preprocess() # 数据预处理，包括检查数据下载情况、生成字典文件
+    code_check() # 代码检查，包括面向用例编程和非Python代码检查
+    code_remove() # 代码移除，将无效代码移至专门文件夹
+    get_nums() # 频数计数，按题目或用户为单位计算多个频数
+    graph_generate() # 图像生成，绘制统计图
     print("分析完成！")
 
